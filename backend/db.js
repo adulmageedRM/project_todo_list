bfc96d961ab4ebb17756f3fa815856d330fd1121
adulmageedRM <abdulmaged-rj@hotmail.com>
2021-12-21 18:45:47 +0300

    setup the project

---
+++ backend/db.js
@@ -0,0 +1,20 @@
+const mongoose=require('mongoose')
+
+const dbURI= "mongodb://localhost:27017/TodolistV01";
+mongoose.connect(dbURI)
+
+
+const db = mongoose.connection;
+
+db.on("error",(err)=>{
+    console.log('ERROR in mongodb');
+});
+
+db.on("connected",(err)=>{
+    console.log('mongodb is CONNECTED');
+});
+
+
+
+
+
