85bacb669e5e1a0708da5d4a60c5c6e13c6d02ba
adulmageedRM <abdulmaged-rj@hotmail.com>
2021-12-22 18:50:56 +0300

    finish get and post

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,44 @@
-const express=require('express')
+const express=require('express');
 const app=express()
+app.use(express.json())
 
-const db=require('./db')
-const Todo=require('./todo')
+const db=require('./db');
+const Todo=require('./todo');
 
 // console.log(Todo)
 
 
 app.get('/',(req,res)=>{
     res.json('GET / is working')
-})
-app.get('/tasks',(req,res)=>{
-    res.json('GET / is working')
-})
+});
 
+
+app.get('/tasks',(req,res)=>{
+    Todo.find({},(err,data)=>{
+        if(err){
+            console.log('ERROR',err)
+        }else{
+            res.json(data)
+        }
+    });
+});
+
+
+app.post('/tasks',(req,res)=>{
+    Todo.create(req.body,(err,newTask)=>{
+        console.log('25',req.body)
+        if(err){
+            console.log('ERROR',err)
+        }else{
+           res.status(201).json(newTask)
+        }
+    });
+});
 
 
 
 
 app.listen(5000,()=>{
     console.log('SERVER is working')
-})
+});
 
