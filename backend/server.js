a8734c01acc9e9d7613a8764ab44c3176b82ea11
adulmageedRM <abdulmaged-rj@hotmail.com>
2021-12-23 23:31:29 +0300

    finish useEffect and map

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 const express=require('express');
-const app=express()
-app.use(express.json())
+const cors=require("cors");
+const app=express();
+app.use(express.json());
+app.use(cors());
 
 const db=require('./db');
 const Todo=require('./todo');
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
+import axios from 'axios';
+import Todo from './components/Todo'
 import './App.css';
 
 export default function App() {
-const getdata=()=>{
   
-}
+  const [tasks, setTasks]=useState([])
 
+  const getdata=()=>{
+    axios
+     .get("http://localhost:5000/tasks")
+     .then((res)=>{
+       console.log("DATA",res.data);
+       setTasks(res.data);
+     })
+     .catch((err)=>{
+       console.log("ERROR",err);
+     });
+  };
+  const mapOverTasks=tasks.map((taskObj,i)=>(
+   <Todo key={i} task={taskObj} />
+  ));
 
   return (
     <div className="App">
      <p>app</p>
      <button onClick={getdata}>GET TASKS</button>
+
+     {mapOverTasks}
     </div>
   );
 }
